145998f50c6ccb729aa2aee809f1e0ee33e0fc5b
Peter Petermann <ppetermann80@googlemail.com>
2015-06-10 13:03:28 +0200

    update to latest psr draft

--- src/Psr/Cache/CacheItemInterface.php
+++ src/Psr/Cache/CacheItemInterface.php
@@ -74,32 +74,31 @@ interface CacheItemInterface
     public function exists();
 
     /**
-     * This method is used to tell future calls to this item if re-regeneration of
-     * this item's data is in progress or not.
+     * Sets the expiration time for this cache item.
      *
-     * This can be used to prevent the dogpile effect to stop lots of requests re-generating
-     * the fresh data over and over.
-     *
-     * @return boolean
-     */
-    public function isRegenerating();
-
-    /**
-     * Sets the expiration for this cache item.
-     *
-     * @param int|\DateTime $ttl
-     *   - If an integer is passed, it is interpreted as the number of seconds
-     *     after which the item MUST be considered expired.
-     *   - If a DateTime object is passed, it is interpreted as the point in
-     *     time after which the item MUST be considered expired.
-     *   - If null is passed, a default value MAY be used. If none is set,
-     *     the value should be stored permanently or for as long as the
-     *     implementation allows.
+     * @param \DateTimeInterface $expiration
+     *   The point in time after which the item MUST be considered expired.
+     *   If null is passed explicitly, a default value MAY be used. If none is set,
+     *   the value should be stored permanently or for as long as the
+     *   implementation allows.
      *
      * @return static
      *   The called object.
      */
-    public function setExpiration($ttl = null);
+    public function expiresAt($expiration);
+
+    /**
+     * Sets the expiration time for this cache item.
+     *
+     * @param int|\DateInterval $time
+     *   The period of time from the present after which the item MUST be considered
+     *   expired. An integer parameter is understood to be the time in seconds until
+     *   expiration.
+     *
+     * @return static
+     *   The called object.
+     */
+    public function expiresAfter($time);
 
     /**
      * Returns the expiration time of a not-yet-expired cache item.
